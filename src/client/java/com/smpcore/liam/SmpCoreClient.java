faaf6d2c0a147624d72e50f05bdc752c4e7948e7
Lianomeister <liam.bollmann@gmx.at>
2026-03-08 01:03:11 +0100

    Initial commit

---
+++ src/client/java/com/smpcore/liam/SmpCoreClient.java
@@ -0,0 +1,10 @@
+package com.smpcore.liam;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SmpCoreClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
